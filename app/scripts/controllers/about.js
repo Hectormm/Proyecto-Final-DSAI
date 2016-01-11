a06cf07c3ac6d737f9bf60cc0a73289ea5cdff1e
Hectormm <hectormm@gmail.com>
2016-01-11 18:10:54 +0100

    Commit inicial

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name nuevaCarpetaApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the nuevaCarpetaApp
+ */
+angular.module('nuevaCarpetaApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name nuevaCarpetaApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the nuevaCarpetaApp
+ */
+angular.module('nuevaCarpetaApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
